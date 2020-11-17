bc4c06fddb66accd56d8cc504353659a2dd793a2
Kelly Danger <kellyanndanger@gmail.com>
2020-11-17 12:17:17 -0600

    checkboxes added with labels, still workingon toggling logic

--- src/components/ActivitySelect/ActivitySelect.js
+++ src/components/ActivitySelect/ActivitySelect.js
@@ -3,10 +3,19 @@ import { connect } from 'react-redux';
 // import mapStoreToProps from '../../redux/mapStoreToProps';
 
 class ActivitySelect extends Component {
+  state = {
+    isChecked: false
+  }
 
   componentDidMount = () => {
     this.props.dispatch({
       type: 'FETCH_ACTIVITIES'
+    })
+  }
+  
+  handleCheck = (event) => {
+    this.setState({
+      isChecked: !this.state.isChecked,
     })
   }
 
@@ -14,7 +23,15 @@ class ActivitySelect extends Component {
     return(
       <div>
         <p>This is where you Select your Activities</p>
-        {JSON.stringify(this.props.reduxState.activityReducer)}
+        {/* {JSON.stringify(this.props.reduxState.activityReducer)} */}
+          {this.props.reduxState.activityReducer.map((activity) => {
+            return (
+            <div key={activity.id}>
+              <input type="checkbox" onChange={this.handleCheck} value={activity.description} id={activity.id}/>
+              <label htmlFor={activity.id}>{activity.description}</label>
+            </div>
+            )
+          })}
       </div>
     )
   }
