f18ad1d011d03244cc1b9063c48395f7157bd9e5
Kelly Danger <kellyanndanger@gmail.com>
2020-11-17 11:52:35 -0600

    activities reducer saga and component retrieving data

--- src/components/ActivitySelect/ActivitySelect.js
+++ src/components/ActivitySelect/ActivitySelect.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import mapStoreToProps from '../../redux/mapStoreToProps';
+// import mapStoreToProps from '../../redux/mapStoreToProps';
 
 class ActivitySelect extends Component {
+
+  componentDidMount = () => {
+    this.props.dispatch({
+      type: 'FETCH_ACTIVITIES'
+    })
+  }
 
   render() {
     return(
       <div>
         <p>This is where you Select your Activities</p>
+        {JSON.stringify(this.props.reduxState.activityReducer)}
       </div>
     )
   }
 }
+const mapStoreToProps = reduxState => ({
+  reduxState
+})
+
 
 export default connect(mapStoreToProps)(ActivitySelect);
---
+++ src/redux/reducers/activity.reducer.js
@@ -0,0 +1,11 @@
+
+const activityReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_ACTIVITIES':
+      return action.payload;
+    default: 
+      return state;
+  }
+}
+
+export default activityReducer;
