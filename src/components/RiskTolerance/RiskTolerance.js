858a9120deb799f8c169587b0b002822becc3c55
Kelly Danger <kellyanndanger@gmail.com>
2020-11-19 12:21:08 -0600

    radio buttons sending tolerance number

--- src/components/RiskTolerance/RiskTolerance.js
+++ src/components/RiskTolerance/RiskTolerance.js
@@ -6,20 +6,26 @@ class RiskTolerance extends Component {
   state = {
     toleranceNum: 0,
   }
+
+  handleCheck = (event, param) => {
+    console.log(param);
+  }
+
+
   render() {
     return(
       <>
         <p>{this.state.toleranceNum}</p>
         <div className="toleranceForm">
-          <input type="radio" id="1" name="tolerance" value="1"/>
+          <input type="radio" id="1" name="tolerance" value="1" onChange={(event) => this.handleCheck(event, '1')} />
           <label htmlFor="1">Level 1 stuff</label><br></br>
-          <input type="radio" id="2" name="tolerance" value="2"/>
+          <input type="radio" id="2" name="tolerance" value="2" onChange={(event) => this.handleCheck(event, '2')}/>
           <label htmlFor="2">Level 2 stuff</label><br></br>
-          <input type="radio" id="3" name="tolerance" value="3"/>
+          <input type="radio" id="3" name="tolerance" value="3" onChange={(event) => this.handleCheck(event, '3')}/>
           <label htmlFor="3">Level 3 stuff</label><br></br>
-          <input type="radio" id="4" name="tolerance" value="4"/>
+          <input type="radio" id="4" name="tolerance" value="4" onChange={(event) => this.handleCheck(event, '4')}/>
           <label htmlFor="4">Level 4 stuff</label><br></br>
-          <input type="radio" id="5" name="tolerance" value="5"/>
+          <input type="radio" id="5" name="tolerance" value="5" onChange={(event) => this.handleCheck(event, '5')}/>
           <label htmlFor="5">Level 5 stuff</label>
         </div>
       </>
