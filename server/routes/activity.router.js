2d9d64953db1e7cf9e1fcbac006989e838decde8
Kelly Danger <kellyanndanger@gmail.com>
2020-11-18 20:02:48 -0600

    post route working will debug later

--- server/routes/activity.router.js
+++ server/routes/activity.router.js
@@ -17,14 +17,13 @@ router.post('/', (req, res) => {
   let activitiesArray = req.body.activitiesArray
   for(let val of activitiesArray) {
     console.log('each item is', val);
-    let queryText = `INSERT INTO "user_activities" ("activity_id", "user_id") VALUES (${val}, ${req.body.userId});`;
-    pool.query(queryText).then((result) => {
-      // res.send(result.rows)
+    let queryText = `INSERT INTO "user_acivities" ("activity_id", "user_id") VALUES (${val}, ${req.body.userId});`;
+    pool.query(queryText).then((result) => {  
   }).catch((error) => {
     console.log('error in post');
     return res.sendStatus(500);
   })
-  }
+  } res.sendStatus(200)
 })
 
 
